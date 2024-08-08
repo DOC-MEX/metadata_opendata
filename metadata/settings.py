767726a7e9e5385c439f2aa8b0a81aaf53385f62
Daniel <marmandu@gmail.com>
2024-08-08 11:58:41 +0000

    add custom_setting files to main settings

--- metadata/settings.py
+++ metadata/settings.py
@@ -19,13 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9j#b%)y3!u=iv@c&74n)i#+#^tk@vetzemy4l&=sw@(wbh@xkj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+      '127.0.0.1'
+#      'grassroots.tools'
+        ]
 
 
 # Application definition
@@ -122,3 +123,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from .custom_settings import *
+except ImportError:
+    pass
+
+
+#FORCE_SCRIPT_NAME = '/opendatadsw'
+#CSRF_TRUSTED_ORIGINS = ['https://grassroots.tools']
